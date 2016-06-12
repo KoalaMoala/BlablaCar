3365bbf5a4f26aa2f7ee17b64364e2dc72cc6961
Nicolas Amelot <nicolas.amelot@outlook.com>
2016-06-12 01:06:26 -0400

    Closes KoalaMoala/BlablaCar#1

--- application/helpers/form_validator_helper.php
+++ application/helpers/form_validator_helper.php
@@ -2,7 +2,7 @@
 function not_null($post_data, $vars){
   foreach($vars as $var)
   {
-    if(!array_key_exists($var, $post_data) )
+    if(!array_key_exists($var, $post_data) || $post_data[$var] == null)
       return FALSE;
   }
 
